990b6941930131161663b272600480b0e3981d35
heyscottn <heyscottn@gmail.com>
2024-05-10 14:12:20 -0500

    mm

---
+++ life_functions.py
@@ -0,0 +1,38 @@
+
+
+# given filename, height, width
+# return boolean 2D list (True=alive)
+def create_grid(filename, height, width):
+    pass
+
+
+# helpful symbols
+# ▫️◻️◼️▪️
+# □■
+# given the grid
+# display in a neat way
+def show_grid(grid):
+def show_grid(grid):
+    pass
+
+
+
+# given the grid
+# return how many cells are 'alive'
+def count_alive(grid):
+    pass
+
+
+
+
+# given the grid and a row, column
+# count how many neighbors are 'alive'
+def count_neighbors(grid, row, col):
+    pass
+
+
+
+# given the grid
+# return the grid 'after' one life cycle
+def one_cycle(grid):
+    pass
